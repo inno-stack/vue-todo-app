fa542f07d98e887c238e449884065f0634dce299
innocent <workinhim@gmail.com>
2025-10-23 19:59:38 +0100

    fixing

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -5,18 +5,13 @@
 // const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 // export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-if (!supabaseUrl || !supabaseAnonKey) {
-  console.error("Supabase environment variables:", {
-    supabaseUrl,
-    supabaseAnonKey,
-  });
-  throw new Error("supabaseUrl and supabaseAnonKey are required.");
+if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
+  throw new Error("Supabase environment variables are missing!");
 }
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
